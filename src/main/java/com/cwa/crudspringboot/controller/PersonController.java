9d231d9b28064fa3111fda483b06c67599816470
allyRx <herindrainyall2003@gmail.com>
2026-01-11 12:14:01 +0300

    add documentation API

--- src/main/java/com/cwa/crudspringboot/controller/PersonController.java
+++ src/main/java/com/cwa/crudspringboot/controller/PersonController.java
@@ -2,6 +2,8 @@ package com.cwa.crudspringboot.controller;
 
 import com.cwa.crudspringboot.entity.Person;
 import com.cwa.crudspringboot.service.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,17 +19,37 @@ public class PersonController {
 
     private final PersonService personService;
 
+
+    @Operation(
+            summary = "Recuperer tous les personnes ",
+            description = "Retournes tous les personnes dans la base des donnes"
+
+    )
+    @ApiResponse(responseCode = "200", description = "Personnes trouve avec succes ")
+    @ApiResponse(responseCode = "404", description = "Personne pas trouver")
     @GetMapping
     public ResponseEntity<List<Person>> getAllPersons() {
         return new ResponseEntity<>(personService.findAll(), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Ajoutez une personne",
+            description = "Pour ajouter une personnes ave son name , city , phoneNumber"
+    )
+    @ApiResponse(responseCode = "201", description = "Personne bien cree")
+    @ApiResponse(responseCode = "500", description = "Internal server Error")
     @PostMapping
     public ResponseEntity<Person> createPerson(@RequestBody Person person) {
         Person personCreated = personService.saveOrUpdate(person);
         return new ResponseEntity<>(personCreated, HttpStatus.CREATED);
     }
 
+
+    @Operation(
+            summary = "recuperer une Personne par son ID"
+    )
+    @ApiResponse(responseCode = "200", description = "Person recuperer par son ID avec succes")
+    @ApiResponse(responseCode = "404", description = "Person pas trouver ")
     @GetMapping("/{id}")
     public ResponseEntity<Person> getPersonById(@PathVariable Long id) {
         Person person = personService.findById(id);
@@ -35,6 +57,10 @@ public class PersonController {
         return Objects.nonNull(person) ? new ResponseEntity<>(person, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @Operation(
+            summary = "Mise a jour d'une personne"
+    )
+    @ApiResponse(responseCode = "201", description = "Person bien mise a jour ")
     @PutMapping("/{id}")
     public ResponseEntity<Person> updatePerson(@PathVariable Long id, @RequestBody Person personDetails) {
         Person existingPerson = personService.findById(id);
@@ -49,6 +75,8 @@ public class PersonController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @Operation(summary = "SUppression Person")
+    @ApiResponse(responseCode = "200", description = "Personne bien supprimer!!")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePerson(@PathVariable Long id) {
         Person person = personService.findById(id);
